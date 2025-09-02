a7644551179b8da759183157a5f38429640131cb
Daniel <azsidaniel@gmail.com>
2025-09-02 11:06:49 -0300

    perf: 📈 vercel speed-insights

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 import 'antd/dist/reset.css'
 
@@ -10,5 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <AppRoutes />
     </BrowserRouter>
+    <SpeedInsights />
   </React.StrictMode>
 )
